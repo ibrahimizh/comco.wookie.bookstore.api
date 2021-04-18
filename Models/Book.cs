797499debe1649ae5ff30d0654b59e642decaa94
ibrahimizh <ibrahim_na@outlook.com>
2021-04-18 12:33:29 -0300

    Added Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+
+namespace comco.wookie.bookstore.api.Models{
+    public class Book{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+        public decimal Price { get; set; }
+    }
+}
