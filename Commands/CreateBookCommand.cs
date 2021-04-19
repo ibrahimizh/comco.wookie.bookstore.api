3980249a84b6328a9a4980101cef59df61ea81b7
ibrahimizh <ibrahim_na@outlook.com>
2021-04-19 00:40:36 -0300

    Code cleanup and added logging

--- Commands/CreateBookCommand.cs
+++ Commands/CreateBookCommand.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using comco.wookie.bookstore.api.Models;
 using comco.wookie.bookstore.api.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace comco.wookie.bookstore.api.Commands
 {
@@ -18,16 +20,19 @@ namespace comco.wookie.bookstore.api.Commands
     {
         private readonly IBooksRepository booksRepository;
         private readonly IMapper mapper;
+        private readonly ILogger<CreateBookHandler> logger;
 
-        public CreateBookHandler(IBooksRepository _booksRepository,IMapper _mapper)
+        public CreateBookHandler(IBooksRepository _booksRepository,IMapper _mapper,ILogger<CreateBookHandler> _logger)
         {
             booksRepository = _booksRepository;
             mapper = _mapper;
+            logger = _logger;
 
         }
         public async Task<BookDTO> Handle(CreateBookCommand request, CancellationToken cancellationToken)
         {
             var book = await booksRepository.AddAsync(mapper.Map<BookDTO,Book>(request,new Book()));
+            logger.LogInformation($"A new book has been added to the repository {JsonConvert.SerializeObject(book)}");
             return mapper.Map<BookDTO>(book);
         }
     }
--- Commands/DeleteBookCommand.cs
+++ Commands/DeleteBookCommand.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using comco.wookie.bookstore.api.Models;
 using comco.wookie.bookstore.api.Repositories;
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace comco.wookie.bookstore.api.Commands
 {
@@ -21,14 +23,19 @@ namespace comco.wookie.bookstore.api.Commands
     public class DeleteBookHandler : IRequestHandler<DeleteBookCommand, bool?>
     {
         private readonly IBooksRepository booksRepository;
+        private readonly ILogger<DeleteBookHandler> logger;
 
-        public DeleteBookHandler(IBooksRepository _booksRepository)
+        public DeleteBookHandler(IBooksRepository _booksRepository,ILogger<DeleteBookHandler> _logger)
         {
             booksRepository = _booksRepository;
+            logger = _logger;
         }
         public async Task<bool?> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
         {
-            return await booksRepository.DeleteAsync(request.Id);
+            var result = await booksRepository.DeleteAsync(request.Id);
+            if(result is null) logger.LogWarning($"A delete request was submitted with a non-existing Id : {request.Id}");
+            else logger.LogInformation($"This book with Id : {request.Id} has been deleted!");
+            return result;
         }
     }
 
